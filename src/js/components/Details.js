c4d58f5c0418d4ca27216517ca44600af0c91151
Radhika V Kathavate <radhika@Venugopals-MacBook-Pro.local>
2016-07-17 13:12:45 +0530

    Images for details section

--- src/js/components/Details.js
+++ src/js/components/Details.js
@@ -12,16 +12,19 @@ var Details = () => {
 	return (
 		<div className="row-fluid details clear-fix">
 			<div className="small-4 columns text-center">
+                <img src="solutions.jpg" alt="This is test image"/>
                 <h3>Solutions</h3>
                 <span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor</span>
                 <LearnMore/>
 			</div>
             <div className="small-4 columns text-center">
+                <img src="resources.gif" alt="This is test image"/>
                 <h3>Resources</h3>
                 <span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</span>
                 <LearnMore/>
 			</div>
             <div className="small-4 columns text-center">
+                <img src="technology.jpg" alt="This is test image"/>
                 <h3>Technology</h3>
                 <span>
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
