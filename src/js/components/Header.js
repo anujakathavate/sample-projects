0f25ef1c741e0b1d54f18c4c5d77d5e28dd29fc5
Radhika V Kathavate <radhika@Venugopals-MacBook-Pro.local>
2016-08-07 13:16:16 +0530

    Mobile and tablet compatibility for Header component.

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,19 +2,35 @@ import React from "react";
 import Navigation from "./Navigation";
 
 export default class Header extends React.Component {
+	// TODO: Revisit this do away with single DOM not separate for desktop and mobile.
 	render() {
 		return (
 			<header className="header">
-				<div className="row">
-					<div className="large-3 text-center columns brand-details">
+				<div className="row hide-for-small-only">
+					<div className="medium-3 large-3 text-center columns brand-details mail">
 						sales@emergesoft.in
 						<br/>
 						Bangalore
 					</div>
-					<div className="large-6 text-center columns">
+					<div className="medium-6 large-6 text-center columns brand">
 						<h1 className="brand-name">EmergeSoft</h1>
 					</div>
-					<div className="large-3 text-center columns brand-details">
+					<div className="medium-3 large-3 text-center columns brand-details phone">
+						+91 994 5333602
+						<br/>
+						+91 973 1100227
+					</div>
+				</div>
+				<div className="row show-for-small-only">
+					<div className="small-12 text-center columns brand">
+						<h1 className="brand-name">EmergeSoft</h1>
+					</div>
+					<div className="small-6 text-center columns brand-details mail">
+						sales@emergesoft.in
+						<br/>
+						Bangalore
+					</div>
+					<div className="small-6 text-center columns brand-details phone">
 						+91 994 5333602
 						<br/>
 						+91 973 1100227
