52492c4da1ddb68c46492ab2effea0bb795f2379
Radhika V Kathavate <radhika@Venugopals-MacBook-Pro.local>
2016-08-14 23:44:02 +0530

    Mobile and tablet compatibility for welcome note

--- src/js/components/WelcomeNote.js
+++ src/js/components/WelcomeNote.js
@@ -3,15 +3,15 @@ import React from "react";
 var WelcomeNote = () => {
 	return (
 		<div className="row-fluid welcome">
-			<div className="small-2 small-centered text-center">
+			<div className="large-3 large-centered text-center">
                 <h3>WELCOME</h3>
 			</div>
-            <div className="small-6 small-centered text-center">
+            <div className="large-6 large-centered text-center">
                 <div className="welcome-note">
                     We build beautiful websites with passion and take all your burdens  of day-today work on us by making the software as easy as possible that even children can play with  and use the software as it it was a toy.
                 </div>
 			</div>
-            <div className="small-2 small-centered text-center">
+            <div className="large-3 large-centered text-center">
                 <button className="">Learn More</button>
 			</div>
 		</div>
