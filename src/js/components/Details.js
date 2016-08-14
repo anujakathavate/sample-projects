313a763dfba54898e456fde62eabb33dfd4676a7
Radhika V Kathavate <radhika@Venugopals-MacBook-Pro.local>
2016-08-14 23:50:27 +0530

    Mobile and tablet compatibility for Details component

--- src/js/components/Details.js
+++ src/js/components/Details.js
@@ -11,19 +11,19 @@ var LearnMore = () => {
 var Details = () => {
 	return (
 		<div className="row-fluid details clear-fix">
-			<div className="small-4 columns text-center">
+			<div className="small-12 medium-4 columns text-center">
                 <img src="solutions.jpg" alt="This is test image"/>
                 <h3>Solutions</h3>
                 <span>We are the single point of contact for all your website needs.</span>
                 <LearnMore/>
 			</div>
-            <div className="small-4 columns text-center">
+            <div className="small-12 medium-4 columns text-center">
                 <img src="resources.gif" alt="This is test image"/>
                 <h3>Resources</h3>
                 <span>We have well qualified and experienced professionals in our organization to develop your websites and also to support you people in future.</span>
                 <LearnMore/>
 			</div>
-            <div className="small-4 columns text-center">
+            <div className="small-12 medium-4 columns text-center">
                 <img src="technology.jpg" alt="This is test image"/>
                 <h3>Technology</h3>
                 <span>
